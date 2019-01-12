269165333f4c544f63498bb4c639d98642ca18b7
JoJettison <jb9013xz@gmail.com>
2019-01-12 09:27:18 -0600

    Put the grid inside the class instead of floating around in namespace land

--- include/grid.hpp
+++ include/grid.hpp
@@ -2,18 +2,22 @@
 #define __GRID_HPP__
 
 #include "val.hpp"
+#include <iostream>
 namespace bsp
 {
     class Grid
     {
     public:
+      Grid();
       void display(int mode);
       void addShip(int fcordx, int fcordy, int ecordx, int ecordy);
       int fire(int xcord, int ycord);
       int alphConvert(char alph);
       int gameactive();
       void nuke();
+    private:
+        std::string battlegrid[11][15];
     };
-  }
+}
 
 #endif
